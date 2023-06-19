e01e92cb60dbf4f0ea54939b1f968289199eb375
DR497 <47689875+dr497@users.noreply.github.com>
2023-06-19 13:35:03 +0800

    minor fix

--- src/utils/validate/index.tsx
+++ src/utils/validate/index.tsx
@@ -5,8 +5,15 @@ export const trimTld = (x: string) => {
   return x;
 };
 
+export const countOccurences = (x: string, char: string): number => {
+  return x.split(char).length - 1;
+};
+
 export const validate = (x: string): boolean => {
   x = trimTld(x);
+  if (countOccurences(x, ".") > 1) {
+    return false;
+  }
   if (x !== x.toLowerCase()) {
     return false;
   }
