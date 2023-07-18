70d83067beeebabba9876cf03d8e77c2c6a63f4a
Dmytro Svyrydenko <dmytro.svyrydenko@gmail.com>
2023-07-18 19:28:25 +0300

    feat: Add src path alias to babel and tsconfig

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,20 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["transform-inline-environment-variables", "macros"],
+    plugins: [
+      "transform-inline-environment-variables",
+      "macros",
+      ["module-resolver", {
+        "alias": {
+          "@src": "./src/",
+        },
+        "extensions": [
+          ".js",
+          ".jsx",
+          ".ts",
+          ".tsx",
+        ]
+      }],
+    ],
   };
 };
