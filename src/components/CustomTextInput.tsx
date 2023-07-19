e8aeab3e003ecc817123994d1f067cd80d4809ff
Dmytro Svyrydenko <dmytro.svyrydenko@gmail.com>
2023-07-19 15:04:35 +0300

    feat: Improve custom UI components

--- src/components/CustomTextInput.tsx
+++ src/components/CustomTextInput.tsx
@@ -2,18 +2,45 @@ import {
   TextInput,
   TextInputProps,
   Platform,
+  View,
+  TouchableOpacity,
 } from "react-native";
+import { Feather } from "@expo/vector-icons";
 import tw from "@src/utils/tailwind";
 
-export const CustomTextInput = (props: TextInputProps) => {
+type TextInputTypes = 'text' | 'search'
+
+export const CustomTextInput = (
+  { type = 'text', ...props }: TextInputProps & {
+    type?: TextInputTypes;
+  }
+) => {
+  const ClearValueAction = () => {
+    if (props.onChangeText && type === 'search' && props.value) {
+      return (
+        <TouchableOpacity
+          onPress={() => props.onChangeText?.('')}
+          style={tw`flex flex-col rounded-lg items-center justify-center h-[38px] px-3 bg-white absolute right-[1px] top-[1px]`}
+        >
+          <Feather name="x" size={16} color="black" />
+        </TouchableOpacity>
+      )
+    }
+    return null
+  }
+
   return (
-    <TextInput
-      {...props}
-      style={[
-        Platform.OS === "web" && { outlineWidth: 0 },
-        tw`bg-white h-full rounded-lg pl-3 font-regular text-content-secondary border border-content-border`,
-      ]}
-      placeholderTextColor="#A3A3A3"
-    />
+    <View style={tw`w-full h-[40px] relative`}>
+      <TextInput
+        {...props}
+        style={[
+          Platform.OS === "web" && { outlineWidth: 0 },
+          tw`bg-white h-full rounded-lg pl-3 text-content-secondary border border-content-border`,
+        ]}
+        placeholderTextColor="#A3A3A3"
+      />
+
+      <ClearValueAction />
+    </View>
   )
 }
--- src/components/UiButton.tsx
+++ src/components/UiButton.tsx
@@ -13,7 +13,10 @@ export const UiButton = (
   return (
     <TouchableOpacity
       {...props}
-      style={tw`bg-brand-primary py-2 rounded-lg flex items-center justify-center`}
+      style={[
+        tw`bg-brand-primary py-2 rounded-lg flex items-center justify-center`,
+        props.disabled && tw`opacity-60`
+      ]}
     >
       {children ? children : (
         <Text style={tw`text-lg leading-6 font-semibold text-white`}>
