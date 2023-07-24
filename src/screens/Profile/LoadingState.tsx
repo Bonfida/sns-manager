326eb88c55913504d88f07e15aab01e0efcb5757
Dmytro Svyrydenko <dmytro.svyrydenko@gmail.com>
2023-07-24 14:11:06 +0300

    chore: Improve loading state

--- src/screens/Profile/LoadingState.tsx
+++ src/screens/Profile/LoadingState.tsx
@@ -7,11 +7,9 @@ export const LoadingState = () => {
   return (
     <View style={tw`flex px-3 py-5 flex-col items-start w-full h-full mt-4`}>
       <View style={tw`w-full relative mb-10`}>
-        {/* <View style={tw`absolute top-[-60px]`}> */}
         <SkeletonContent isLoading>
-          <View style={tw`w-[100px] h-[100px] mb-2 rounded-full`} />
+          <View style={tw`w-[100px] h-[100px] mb-2 rounded-full absolute top-[-25px]`} />
         </SkeletonContent>
-        {/* </View> */}
         <SkeletonContent isLoading>
           <View style={tw`h-[140px] w-full rounded-lg`}/>
         </SkeletonContent>
