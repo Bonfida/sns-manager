836a408369b6cde2d5a7a8a1326c0f346dedfde8
Dmytro Svyrydenko <dmytro.svyrydenko@gmail.com>
2023-07-25 16:53:38 +0300

    feat: Improve button component style

--- src/components/UiButton.tsx
+++ src/components/UiButton.tsx
@@ -2,13 +2,16 @@ import {
   Text,
   TouchableOpacity,
   TouchableOpacityProps,
+  ActivityIndicator,
 } from "react-native";
 import tw from "@src/utils/tailwind";
 import { ReactNode } from "react";
 
 export const UiButton = (
-  { content, outline = false, children, ...props }:
+  { small, content, outline = false, loading = false, children, ...props }:
   TouchableOpacityProps & {
+    small?: boolean;
+    loading?: boolean;
     content?: ReactNode,
     children?: ReactNode,
     outline?: boolean,
@@ -18,20 +21,31 @@ export const UiButton = (
     <TouchableOpacity
       {...props}
       style={[
-        tw`py-2 rounded-lg flex items-center justify-center border-brand-primary`,
-        outline && tw`border-2`,
+        tw`rounded-lg flex flex-1 items-center justify-center border-2 border-brand-primary`,
+        small && tw`py-0.5`,
+        !small && tw`py-1.5`,
         !outline && tw`bg-brand-primary`,
         props.disabled && tw`opacity-60`,
         props.style,
       ]}
     >
-      {children ? children : (
+
+      {!content ? (
+        <>
+          {children}
+          {loading && <ActivityIndicator style={tw`ml-3`} size={16} color={outline ? tw.color('brand-primary') : tw.color("white")} /> }
+        </>
+      ) : (
         <Text style={[
-          tw`text-lg leading-6 font-semibold`,
+          tw`font-semibold`,
+          small && tw`text-base`,
+          !small && tw`text-lg leading-6`,
           outline && tw`text-brand-primary`,
           !outline && tw`text-white`,
         ]}>
           {content}
+          {loading && <ActivityIndicator style={tw`ml-3`} size={16} color={outline ? tw.color('brand-primary') : tw.color("white")} /> }
+          {children}
         </Text>
       )}
     </TouchableOpacity>
